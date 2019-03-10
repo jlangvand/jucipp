1244f1a32fc62dcb28c49fbb1129e6cb3efdf880
Jørgen Lien Sellæg <j.sellaeg@sportradar.com>
2019-03-10 16:54:55 +0100

    fix python home and path on windows

--- src/python_interpreter.cc
+++ src/python_interpreter.cc
@@ -27,5 +27,13 @@ Python::Interpreter::~Interpreter() {
 }
 
 Python::Interpreter::Interpreter() {
+#ifdef PYTHON_HOME_DIR
+#ifdef _WIN32
+  const std::wstring python_home(PYTHON_HOME_DIR);
+  const std::wstring python_path(python_home + L";" + python_home + L"\\lib-dynload;" + python_home + L"\\site-packages" );
+  Py_SetPythonHome(python_home.c_str());
+  Py_SetPath(python_path.c_str());
+#endif
+#endif
   py::initialize_interpreter();
 }
