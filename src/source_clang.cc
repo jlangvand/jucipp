d9fe88d1a733de86a7fc50a1ff34282f17cb6430
eidheim <eidheim@gmail.com>
2019-09-16 09:44:20 +0200

    Slight improvement to C/C++ include completion

--- src/source_clang.cc
+++ src/source_clang.cc
@@ -853,7 +853,7 @@ Source::ClangViewAutocomplete::ClangViewAutocomplete(const boost::filesystem::pa
       }
     }
     // Do not insert last " or > inside #include statements
-    if(!row.empty() && ((row.back() == '"' && row.find('"') == row.size() - 1) || (row.back() == '>' && row.find('<') == std::string::npos)))
+    if(!row.empty() && ((*iter == '"' && row.back() == '"' && row.find('"') == row.size() - 1) || (*iter == '>' && row.back() == '>' && row.find('<') == std::string::npos)))
       row.pop_back();
 
     get_buffer()->insert(CompletionDialog::get()->start_mark->get_iter(), row);
