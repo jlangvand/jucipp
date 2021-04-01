18aca81cf61cabf605453cb77589315d6e9ea1ab
eidheim <eidheim@gmail.com>
2021-04-01 20:47:51 +0200

    Added undefined-sanitizer job to CI

--- tests/stubs/project.cpp
+++ tests/stubs/project.cpp
@@ -3,3 +3,19 @@
 std::shared_ptr<Project::Base> Project::current;
 
 std::shared_ptr<Project::Base> Project::create() { return nullptr; }
+
+std::pair<std::string, std::string> Project::Base::get_run_arguments() {
+  return std::make_pair<std::string, std::string>("", "");
+}
+
+void Project::Base::compile() {}
+
+void Project::Base::compile_and_run() {}
+
+void Project::Base::recreate_build() {}
+
+std::pair<std::string, std::string> Project::Base::debug_get_run_arguments() {
+  return std::make_pair<std::string, std::string>("", "");
+}
+
+void Project::Base::debug_start() {}
